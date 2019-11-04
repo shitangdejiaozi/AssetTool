fd70352bed58add012a4f5a1c73ed5d74728ca25
zhangxulei <shitangdejiaozi@163.com>
2019-11-04 16:26:44 +0800

    添加bundlelist的结构

---
+++ AssetTool/Assets/Editor/Scripts/AssetBundleList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleList 
+{
+    public string Platform = string.Empty; 
+    public int TotalCount = 0;
+    public List<AssetBundleInfo> BundleList = new List<AssetBundleInfo>();
+
+    public class AssetBundleInfo
+    {
+        public string FileName = string.Empty;
+        public string Md5Name = string.Empty;
+        private AssetBundle _assetBundle = null;
+        public string[] DependNames = null;
+        public int[] DependIndex = null;
+
+        public AssetBundle assetBundle
+        {
+            get
+            {
+                return _assetBundle;
+            }
+            set
+            {
+                _assetBundle = value;
+            }
+        }
+
+    }
+
+
+}
