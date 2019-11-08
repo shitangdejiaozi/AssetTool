eff4d559078111429ebd26a78c8bc839562344e6
zhangxulei <shitangdejiaozi@163.com>
2019-11-08 17:56:19 +0800

    添加拷贝文件的脚本；添加测试资源

--- AssetTool/BuildScript/pythonFile/CommonFunc.py
+++ AssetTool/BuildScript/pythonFile/CommonFunc.py
@@ -3,10 +3,14 @@ import os
 import sys
 import shutil
 
+def delFile(file):
+    if os.path.exists(file):
+        os.remove(file)
+
 def createDir(dir):
     os.makedirs(dir)
 
-#会把软连接的源文件删除
+#会把软连接的源文件删除,所以不能用来删除软连接目录
 def delDir(dir):
     if os.path.exists(dir):
         shutil.rmtree(dir)
@@ -30,6 +34,32 @@ def getDir(path):
     dir = os.path.dirname(path)
     dir = dir.replace("\\", "/")
     return dir
+
+#适用于destDir未创建
+def CopyDirImmediate(srcDir, destDir):
+    if not os.path.exists(srcDir):
+        return
+    shutil.copytree(srcDir, destDir)
+
+def CopyDir(srcDir, destDir):
+    if not os.path.exists(srcDir):
+        return
+    if not os.path.exists(destDir):
+        os.makedirs(destDir)
+    files = os.listdir(srcDir)
+    for file in files:
+        srcPath = os.path.join(srcDir, file)
+        destPath = os.path.join(destDir, file)
+        if os.path.isfile(srcPath):
+            shutil.copy(srcPath, destPath)
+        else:
+            CopyDir(srcPath, destPath)
+
+def CopyFile(srcFile, destFile):
+    if not os.path.exists(srcFile):
+        return
+    delFile(destFile)
+    shutil.copyfile(srcFile, destFile)
 
 def trimStr(s):
     s = s.rstrip(' ')
--- AssetTool/BuildScript/pythonFile/ExportProj.py
+++ AssetTool/BuildScript/pythonFile/ExportProj.py
@@ -1,8 +1,13 @@
+#encoding=utf-8
 import  os
 import sys
 import CommonFunc
 
 LINK_FILE="build_link_list.txt"
+STREAMING_ASSETS="Assets/StreamingAssets"
+PROJECTSET="ProjectSettings"
+#有些文件需要调整的， 需要移动到StreamingAssets下，打包到apk中，按需添加
+CopyDict = {"Assets/StreamingAssets":"", "ProjectSettings":"","Assets/Resources/data":"Assets/StreamingAssets/data"}
 
 def clearDestProj(dest, isClearLib):
     if not os.path.exists(dest) :
@@ -19,8 +24,12 @@ def clearDestProj(dest, isClearLib):
                 else :
                     CommonFunc.forceDelDir(path)
 
-
-
+#拷贝一些需要更新，但是不需要打ab的数据， 需要移动到Streamingasset下； 后续需要修改的目录
+def CopyProj(srcProj, desetProj):
+    for src, dest in CopyDict.items() :
+        if dest == "":
+            dest = src
+        CommonFunc.CopyDir(os.path.join(srcProj, src), os.path.join(destProj, dest))
 
 def linkProj(srcProj, destProj):
     linkDirs = CommonFunc.readListFromFile(LINK_FILE)
@@ -38,6 +47,7 @@ def exportProj(srcProj, destProj, platform):
     clearDestProj(destProj, True)
     return
     linkProj(srcProj, destProj)
+    CopyProj(srcProj, destProj)
 
 if "__main__" == __name__ :
     srcProj = CommonFunc.GetFuncArg("-srcProj")
